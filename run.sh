c0ef45c03d9f5093fb01c2e0f35a8e98f79c4585
yo far <164857527+yofaras@users.noreply.github.com>
2024-03-25 15:19:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-7412ac94-6f6c-4486-a385-92f6caf8955c/)
 
 
 echo "$curl_output" >> Update.tsx
